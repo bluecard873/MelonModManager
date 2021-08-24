1dbaf8ad9aaa9fc42578e9d6b9e6da7e6fc021e3
Flower <68172262+NoBrain0917@users.noreply.github.com>
2021-08-24 16:46:43 +0900

    Add MLMod, MelonDescriptionAttribute

---
+++ MelonModManager/Util/Attributes/MelonDescriptionAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MelonModManager.Util.Attributes
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class MelonDescriptionAttribute : Attribute
+    {
+        public string Description { get; set; }
+        
+        public MelonDescriptionAttribute(string description = null) 
+        {
+            Description = description;
+        }
+        
+    }
+}
---
+++ MelonModManager/Util/MLMod.cs
@@ -0,0 +1,27 @@
+﻿using MelonLoader;
+using MelonModManager.Util.Attributes;
+
+namespace MelonModManager.Util
+{
+    public class MLMod: MelonMod
+    {
+        public string Description
+        {
+            get
+            {
+                var attribute =
+                    Assembly.GetCustomAttributes(typeof(MelonDescriptionAttribute), false)[0] as
+                        MelonDescriptionAttribute;
+                return attribute?.Description;
+            }
+        }
+
+        public override void OnApplicationStart()
+        {
+            HarmonyInstance.UnpatchAll();
+        }
+
+        public virtual void OnToggle(bool result) { }
+        public virtual void OnSettingGUI() { }
+    }
+}
